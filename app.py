bf58f66f1ea50a8e943dfded3f0e21f5f136fc97
Qing Feng <fengqingthu@gmail.com>
2023-02-07 18:29:46 -0500

    Switch to python

---
+++ app.py
@@ -0,0 +1,18 @@
+from climategpt import get_response
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return get_response(userText)
+
+
+if __name__ == "__main__":
+    app.run()
