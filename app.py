b7d8f2dcd7c43630eeeb53e580fa4c748dee8340
Qing Feng <fengqingthu@gmail.com>
2023-02-12 23:08:29 -0500

    prototype image generation

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from climategpt import get_response
+from stable_diffusion_deepai import get_image
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/get")
+
+@app.route("/chat", methods=["GET"])
 def get_bot_response():
     userText = request.args.get('msg')
     return get_response(userText)
+
+
+@app.route("/image", methods=["GET"])
+def generate_image():
+    prompt = request.args.get('prompt')
+    return get_image(prompt)
 
 
 if __name__ == "__main__":
